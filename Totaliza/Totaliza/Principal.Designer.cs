8530ad86a0eecd2fb2fa8dcb9ae0a3dcba8be1e2
codelogan <codelogan@gmail.com>
2018-10-22 19:29:08 -0300

    Exibe numero da linha ao exportar dados para o Excel

--- Totaliza/Totaliza/Principal.Designer.cs
+++ Totaliza/Totaliza/Principal.Designer.cs
@@ -28,29 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Totalizar = new System.Windows.Forms.Button();
             this.sair = new System.Windows.Forms.Button();
             this.txtCaminhoXML = new System.Windows.Forms.TextBox();
             this.LblTotal = new System.Windows.Forms.Label();
             this.ofd1 = new System.Windows.Forms.OpenFileDialog();
             this.AbrePasta = new System.Windows.Forms.Button();
             this.dgvHashCupons = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.lblMostrar = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHashCupons)).BeginInit();
             this.SuspendLayout();
             // 
-            // Totalizar
-            // 
-            this.Totalizar.Location = new System.Drawing.Point(12, 286);
-            this.Totalizar.Name = "Totalizar";
-            this.Totalizar.Size = new System.Drawing.Size(63, 35);
-            this.Totalizar.TabIndex = 0;
-            this.Totalizar.Text = "Totalizar";
-            this.Totalizar.UseVisualStyleBackColor = true;
-            this.Totalizar.Click += new System.EventHandler(this.Totalizar_Click);
-            // 
             // sair
             // 
-            this.sair.Location = new System.Drawing.Point(427, 286);
+            this.sair.Location = new System.Drawing.Point(499, 279);
             this.sair.Name = "sair";
             this.sair.Size = new System.Drawing.Size(69, 34);
             this.sair.TabIndex = 1;
@@ -60,19 +52,20 @@
             // 
             // txtCaminhoXML
             // 
-            this.txtCaminhoXML.Location = new System.Drawing.Point(12, 23);
+            this.txtCaminhoXML.Location = new System.Drawing.Point(159, 12);
             this.txtCaminhoXML.Name = "txtCaminhoXML";
-            this.txtCaminhoXML.Size = new System.Drawing.Size(360, 20);
+            this.txtCaminhoXML.Size = new System.Drawing.Size(471, 20);
             this.txtCaminhoXML.TabIndex = 3;
             // 
             // LblTotal
             // 
             this.LblTotal.AutoSize = true;
-            this.LblTotal.Location = new System.Drawing.Point(104, 297);
+            this.LblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTotal.Location = new System.Drawing.Point(216, 287);
             this.LblTotal.Name = "LblTotal";
-            this.LblTotal.Size = new System.Drawing.Size(35, 13);
+            this.LblTotal.Size = new System.Drawing.Size(48, 16);
             this.LblTotal.TabIndex = 5;
-            this.LblTotal.Text = "label1";
+            this.LblTotal.Text = "Total:";
             // 
             // ofd1
             // 
@@ -80,9 +73,9 @@
             // 
             // AbrePasta
             // 
-            this.AbrePasta.Location = new System.Drawing.Point(378, 23);
+            this.AbrePasta.Location = new System.Drawing.Point(642, 11);
             this.AbrePasta.Name = "AbrePasta";
-            this.AbrePasta.Size = new System.Drawing.Size(118, 19);
+            this.AbrePasta.Size = new System.Drawing.Size(118, 22);
             this.AbrePasta.TabIndex = 6;
             this.AbrePasta.Text = "Abrir pasta";
             this.AbrePasta.UseVisualStyleBackColor = true;
@@ -91,25 +84,57 @@
             // dgvHashCupons
             // 
             this.dgvHashCupons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHashCupons.Location = new System.Drawing.Point(12, 61);
+            this.dgvHashCupons.Location = new System.Drawing.Point(12, 38);
             this.dgvHashCupons.Name = "dgvHashCupons";
-            this.dgvHashCupons.Size = new System.Drawing.Size(994, 207);
+            this.dgvHashCupons.Size = new System.Drawing.Size(851, 222);
             this.dgvHashCupons.TabIndex = 8;
-            this.dgvHashCupons.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvHashCupons_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(93, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Caminho:";
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.Location = new System.Drawing.Point(394, 279);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(65, 34);
+            this.btnExportar.TabIndex = 10;
+            this.btnExportar.Text = "Exportar";
+            this.btnExportar.UseVisualStyleBackColor = true;
+            this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
+            // 
+            // lblMostrar
+            // 
+            this.lblMostrar.AutoSize = true;
+            this.lblMostrar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMostrar.Location = new System.Drawing.Point(271, 329);
+            this.lblMostrar.Name = "lblMostrar";
+            this.lblMostrar.Size = new System.Drawing.Size(297, 15);
+            this.lblMostrar.TabIndex = 11;
+            this.lblMostrar.Text = "Selecione a pasta onde se encontram os arquivos XML SAT.";
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 332);
+            this.ClientSize = new System.Drawing.Size(876, 353);
+            this.Controls.Add(this.lblMostrar);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvHashCupons);
             this.Controls.Add(this.AbrePasta);
             this.Controls.Add(this.LblTotal);
             this.Controls.Add(this.txtCaminhoXML);
             this.Controls.Add(this.sair);
-            this.Controls.Add(this.Totalizar);
             this.Name = "Principal";
-            this.Text = "Form1";
+            this.Text = "Totalizador de Cupons fiscais SAT";
+            this.Load += new System.EventHandler(this.Principal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHashCupons)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -117,14 +142,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button Totalizar;
         private System.Windows.Forms.Button sair;
         private System.Windows.Forms.TextBox txtCaminhoXML;
         private System.Windows.Forms.Label LblTotal;
         private System.Windows.Forms.OpenFileDialog ofd1;
         private System.Windows.Forms.Button AbrePasta;
         private System.Windows.Forms.DataGridView dgvHashCupons;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Label lblMostrar;
     }
 }
 
--- Totaliza/Totaliza/Principal.cs
+++ Totaliza/Totaliza/Principal.cs
@@ -12,41 +12,25 @@ using System.Security;
 using System.IO;
 using System.Globalization;
 using System.Collections.Generic;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
 
 namespace Totaliza
 {
     public partial class Principal : Form
     {
+        public List<DadosCupom> SortedList;
+        string Pasta;
+
         public Principal()
         {
             InitializeComponent();
         }
 
-        private void Totalizar_Click(object sender, EventArgs e)
-        {
-            XmlTextReader reader = new XmlTextReader(txtCaminhoXML.Text);
-            ArrayList elementos = new ArrayList();
-            string valor;
-
-            while ((reader.Read()))
-            {
-                if (reader.NodeType== XmlNodeType.Element && reader.Name == "vCFe")
-                {
-                    valor = reader.ReadString();
-                    LblTotal.Text = valor;
-                    DadosCupom dc = new DadosCupom();
-                    //dc.valor = System.Convert.ToDecimal(valor);
-
-                }
-            }
-
-        }
-
-
         private void AbrePasta_Click(object sender, EventArgs e)
         {
 
-            string Pasta;
+            
             string valor = "0";
             string dtemissao = "";
             string hremissao = "";
@@ -79,10 +63,11 @@ namespace Totaliza
 
             if (dr == System.Windows.Forms.DialogResult.OK)
             {
-                Pasta = Path.GetDirectoryName(ofd1.FileName);                
+                Pasta = Path.GetDirectoryName(ofd1.FileName);
                 DirectoryInfo Dir = new DirectoryInfo(Pasta);
-                FileInfo[] Files = Dir.GetFiles("*", SearchOption.AllDirectories);
-
+                FileInfo[] Files = Dir.GetFiles("*", SearchOption.AllDirectories).OrderBy(p => p.CreationTimeUtc).ToArray();
+                //Array.Sort(Files, (x, y) => Comparer<DateTime>.Default.Compare(x.CreationTime, y.CreationTime));
+                txtCaminhoXML.Text = Pasta.ToString();
                 // Le os arquivos selecionados 
                 foreach (FileInfo File in Files)
                 {
@@ -92,16 +77,16 @@ namespace Totaliza
                     {
                         XmlTextReader reader = new XmlTextReader(File.FullName);
                         //ArrayList elementos = new ArrayList();
-                         valor = "0";
-                         dtemissao = "";
-                         hremissao = "";
-                         numerocupom = "0";
-                         id = "";
-                        chcanc = "";                       
-                        varValor=0;
-                        varNumeroCupom=0;
-                        varId="";
-                        varChCanc="";
+                        valor = "0";
+                        dtemissao = "";
+                        hremissao = "";
+                        numerocupom = "0";
+                        id = "";
+                        chcanc = "";
+                        varValor = 0;
+                        varNumeroCupom = 0;
+                        varId = "";
+                        varChCanc = "";
 
                         while ((reader.Read()))
                         {
@@ -111,26 +96,26 @@ namespace Totaliza
                                 switch (reader.Name)
                                 {
                                     case "vCFe":
-                                         valor = reader.ReadString();
+                                        valor = reader.ReadString();
                                         break;
                                     case "dEmi":
-                                         dtemissao = reader.ReadString();
+                                        dtemissao = reader.ReadString();
                                         break;
                                     case "hEmi":
-                                         hremissao = reader.ReadString();
+                                        hremissao = reader.ReadString();
                                         break;
                                     case "nCFe":
-                                         numerocupom = reader.ReadString();
+                                        numerocupom = reader.ReadString();
                                         break;
                                     case "infCFe":
                                         id = reader.GetAttribute("Id");
                                         chcanc = reader.GetAttribute("chCanc") ?? "";
-                                        break; 
+                                        break;
                                 }
                             }
                         }
-                        DateTimeFormatInfo usDtfi = new CultureInfo("pt-PT",false).DateTimeFormat;
-                        varData = Convert.ToDateTime(dtemissao.Substring(6, 2) + "/" +  dtemissao.Substring(4, 2) + "/" + dtemissao.Substring(0,4) + " " + hremissao.Substring(0, 2) + ":" + hremissao.Substring(2, 2) + ":" + hremissao.Substring(4, 2), usDtfi);
+                        DateTimeFormatInfo usDtfi = new CultureInfo("pt-PT", false).DateTimeFormat;
+                        varData = Convert.ToDateTime(dtemissao.Substring(6, 2) + "/" + dtemissao.Substring(4, 2) + "/" + dtemissao.Substring(0, 4) + " " + hremissao.Substring(0, 2) + ":" + hremissao.Substring(2, 2) + ":" + hremissao.Substring(4, 2), usDtfi);
                         varValor = Convert.ToDecimal(valor, new CultureInfo("en-US"));
                         varNumeroCupom = Convert.ToInt32(numerocupom);
                         varId = Convert.ToString(id);
@@ -155,13 +140,13 @@ namespace Totaliza
                         {
                             if (hashtableCupons.ContainsKey(varId))
                             {
-                               ((DadosCupom)hashtableCupons[varId]).Numero = varNumeroCupom;
-                               ((DadosCupom)hashtableCupons[varId]).Data = varData;
-                               ((DadosCupom)hashtableCupons[varId]).Valor = varValor;
+                                ((DadosCupom)hashtableCupons[varId]).Numero = varNumeroCupom;
+                                ((DadosCupom)hashtableCupons[varId]).Data = varData;
+                                ((DadosCupom)hashtableCupons[varId]).Valor = varValor;
                             }
                             else
                             {
-                               hashtableCupons.Add(varId, new DadosCupom(varNumeroCupom, varData, varValor, varId, varChCanc));
+                                hashtableCupons.Add(varId, new DadosCupom(varNumeroCupom, varData, varValor, varId, varChCanc));
                             }
                         }
 
@@ -181,18 +166,21 @@ namespace Totaliza
                     }
                 }
                 //LblTotal.Text = Convert.ToString(lCupons.Sum(item => item.Valor));
-                LblTotal.Text = Convert.ToString(DadosCupom.Soma(hashtableCupons));
+                LblTotal.Text = "Total: " + Convert.ToString(DadosCupom.Soma(hashtableCupons));
 
 
 
 
 
                 List<DadosCupom> hashToList = new List<DadosCupom>();
-                foreach(DictionaryEntry de in hashtableCupons)
+                foreach (DictionaryEntry de in hashtableCupons)
                 {
                     hashToList.Add((DadosCupom)de.Value);
                 }
-                var bindingList = new BindingList<DadosCupom>(hashToList);
+                //Teoricamente ordenar aqui
+                //List<DadosCupom> SortedList = hashToList.OrderBy(o => o.Data).ToList();
+                SortedList = hashToList.OrderBy(o => o.Data).ToList();
+                var bindingList = new BindingList<DadosCupom>(SortedList);
                 var source1 = new BindingSource(bindingList, null);
                 dgvHashCupons.DataSource = source1;
                 dgvHashCupons.AutoResizeColumns();
@@ -217,9 +205,98 @@ namespace Totaliza
             this.Close();
         }
 
-        private void dgvHashCupons_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void Principal_Load(object sender, EventArgs e)
         {
 
+        }
+        public void Exportar()
+        {
+            decimal total = 0;
+
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+
+            //Excel.Range formatRange;
+            int linha = 1;
+
+            //Verificar se o excel está instalado
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel não está instalado!");
+                return;
+            }
+            //Cria o workbook
+            xlWorkBook = xlApp.Workbooks.Add(Type.Missing);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            //xlWorkSheet.Range[xlWorkSheet.Cells[linha, 1], xlWorkSheet.Cells[linha, 7]].Style.Locked = false;
+            //Redimensionando coluna de nome do grupo
+            ((Excel.Range)xlWorkSheet.Cells[linha, 1]).EntireColumn.ColumnWidth = 20;
+            ((Excel.Range)xlWorkSheet.Cells[linha, 2]).EntireColumn.ColumnWidth = 15;
+            ((Excel.Range)xlWorkSheet.Cells[linha, 3]).EntireColumn.ColumnWidth = 10;
+            ((Excel.Range)xlWorkSheet.Cells[linha, 4]).EntireColumn.ColumnWidth = 50;
+            ((Excel.Range)xlWorkSheet.Cells[linha, 5]).EntireColumn.ColumnWidth = 50;
+            xlWorkSheet.Cells[linha, 1].NumberFormat = "dd/mm/yyyy hh:mm:ss";
+            xlWorkSheet.Cells[linha, 1] = "Data hora";
+            xlWorkSheet.Cells[linha, 2].NumberFormat = "@";
+            xlWorkSheet.Cells[linha, 2] = "N. Cupom";
+            xlWorkSheet.Cells[linha, 3].NumberFormat = "#,##0.00";
+            xlWorkSheet.Cells[linha, 3] = "Valor";
+            xlWorkSheet.Cells[linha, 3].Style.HorizontalAlignment = Excel.XlHAlign.xlHAlignRight;
+            xlWorkSheet.Cells[linha, 4].NumberFormat = "@";
+            xlWorkSheet.Cells[linha, 4] = "Chave de acesso";
+            xlWorkSheet.Cells[linha, 5].NumberFormat = "@";
+            xlWorkSheet.Cells[linha, 5] = "Chave de cancelamento";
+            linha++;
+
+            
+
+            //List<DadosCupom> lProdutos = Produto.CarregaDadosProduto();
+
+            foreach (DadosCupom dc in SortedList)
+            {
+                xlWorkSheet.Cells[linha, 1] = dc.Data;
+                xlWorkSheet.Cells[linha, 2] = dc.Numero;
+                xlWorkSheet.Cells[linha, 3] = dc.Valor.ToString("0.00");
+                xlWorkSheet.Cells[linha, 4] = dc.id;
+                xlWorkSheet.Cells[linha, 5] = dc.chcanc;
+                if (string.IsNullOrWhiteSpace(dc.chcanc))
+                {
+                    total = total + dc.Valor;
+                }
+                linha++;
+                lblMostrar.Text = "Exportando dados linha: " + linha;
+            }
+            xlWorkSheet.Cells[linha, 1].EntireRow.Font.Bold = true;
+            xlWorkSheet.Cells[linha, 2] = "Total";
+            xlWorkSheet.Cells[linha, 3] = total.ToString("0.00");
+            xlApp.ActiveWindow.SplitRow = 1;
+            //xlApp.ActiveWindow.SplitColumn = 1;
+            xlApp.ActiveWindow.FreezePanes = true;
+
+            //xlWorkSheet.Protect(Type.Missing, xlWorkSheet.ProtectDrawingObjects,  true, xlWorkSheet.ProtectScenarios, xlWorkSheet.ProtectionMode,  xlWorkSheet.Protection.AllowFormattingCells,  xlWorkSheet.Protection.AllowFormattingColumns,  xlWorkSheet.Protection.AllowFormattingRows,  xlWorkSheet.Protection.AllowInsertingColumns,  xlWorkSheet.Protection.AllowInsertingRows,  xlWorkSheet.Protection.AllowInsertingHyperlinks,  xlWorkSheet.Protection.AllowDeletingColumns,  xlWorkSheet.Protection.AllowDeletingRows,  xlWorkSheet.Protection.AllowSorting,  xlWorkSheet.Protection.AllowFiltering,  xlWorkSheet.Protection.AllowUsingPivotTables);
+            //xlWorkSheet.EnableSelection = Excel.XlEnableSelection.xlUnlockedCells;
+            //xlWorkSheet.Protect(Password: "luol2007", AllowFormattingCells: false);
+            xlWorkSheet.Protect("luol2007", true, true);
+            xlWorkBook.SaveAs(Pasta + "\\CuponsSAT.xlsx");
+            xlWorkBook.Close(true, Type.Missing, Type.Missing);
+            xlApp.Quit();
+            //Interop marshaling governs how data is passed in method arguments and return values between managed 
+            //and unmanaged memory during calls. Most data types have common representations in both managed 
+            //and unmanaged memory. The interop marshaler handles these types for you. 
+            //Other types can be ambiguous or not represented at all in managed memory.
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+            lblMostrar.Text = "Exportação dos dados concluida!";
+            MessageBox.Show("Exportação dos dados concluida!", "Aviso");
+         
+        }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+            Exportar();
         }
     }
 }
